dce475544735417b29787d61780258d6e71ff6b8
Andrew Mitchell <andrewpmitchell7@gmail.com>
2018-06-18 12:53:14 -0400

    Implemented all bee classes

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,15 @@
 var Grub = function() {
+  this.age = 0
+  this.color = "pink";
+  this.food = "jelly";
+
 };
 
+Grub.prototype.eat = function() {
+
+};
+
+// Grub.prototype = Object.create(Bee.prototype);
+// Grub.prototype.constructor = Grub;
+// Grub.prototype.eat = function() {
+// };
